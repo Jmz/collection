b16c0ebbc7eff547043feda5287bf50af02201d9
Jmz <jamesowers@gmail.com>
2015-11-14 17:40:56 +0000

    Started adding initial tests

--- Spec/CollectionSpec.php
+++ Spec/CollectionSpec.php
@@ -8,16 +8,28 @@ use Prophecy\Argument;
 
 class CollectionSpec extends ObjectBehavior
 {
+    private $dummyClass = 'Spec\Moltin\Collection\Dummy';
+
     public function let()
     {
         $this->beAnInstanceOf('Spec\Moltin\Collection\DummyCollection');
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
+        $this->beConstructedWith($this->dummyClass);
         $this->shouldHaveType('Spec\Moltin\Collection\DummyCollection');
     }
+
+    public function it_throws_an_exception_if_class_type_does_not_exist()
+    {
+        $this->shouldThrow('\InvalidArgumentException')
+            ->during('__construct', ['Invalid']);
+    }
+
 }
 
 
 class DummyCollection extends Collection {}
+
+class Dummy {}
--- src/Collection.php
+++ src/Collection.php
@@ -4,5 +4,10 @@ namespace Moltin\Collection;
 
 abstract class Collection
 {
-
+    public function __construct($type)
+    {
+        if (!class_exists($type)) {
+            throw new \InvalidArgumentException("{$type} is not a valid class");
+        }
+    }
 }
